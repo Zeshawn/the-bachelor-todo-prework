53873e756f98d1a3030b6fcc79ccfd873b3185cd
Zeshawn Khan <zeshawnk@gmail.com>
2018-09-12 13:48:30 +0000

    automatically backed up by learn

--- lib/bachelor.rb
+++ lib/bachelor.rb
@@ -1,9 +1,26 @@
 def get_first_name_of_season_winner(data, season)
   # code here
+  data[season].each do |people|
+       people.each do |k,v|
+        if v == "Winner"
+            full_name = people["name"]
+            return full_name.split(' ').first
+        end 
+      end
+    end
 end
 
 def get_contestant_name(data, occupation)
   # code here
+  data.each do |season, array|
+    array.each do |people|
+      people.each do |k,v|
+        if v == occupation
+          return people["name"]
+        end
+      end
+    end
+  end
 end
 
 def count_contestants_by_hometown(data, hometown)
